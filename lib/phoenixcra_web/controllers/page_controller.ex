a58973c1946c9fd91cce0670f0eff900deda7ad6
iceniveth <ken11zer01@gmail.com>
2019-12-24 15:31:18 +0800

    Serve all static files from / route

--- lib/phoenixcra_web/controllers/page_controller.ex
+++ lib/phoenixcra_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule PhoenixcraWeb.PageController do
   use PhoenixcraWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    html(conn, File.read!("./priv/static/index.html"))
   end
 end
--- lib/phoenixcra_web/endpoint.ex
+++ lib/phoenixcra_web/endpoint.ex
@@ -11,9 +11,7 @@ defmodule PhoenixcraWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
-    from: :phoenixcra,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    from: :phoenixcra
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
