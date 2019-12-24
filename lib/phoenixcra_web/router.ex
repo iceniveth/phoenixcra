3612ccfb6d9f6a0bf188743ae10ef937dc9785a9
iceniveth <ken11zer01@gmail.com>
2019-12-24 15:32:55 +0800

    Redirect to client side routes when no route is matched

--- lib/phoenixcra_web/router.ex
+++ lib/phoenixcra_web/router.ex
@@ -17,6 +17,7 @@ defmodule PhoenixcraWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get("/*path", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
